ca9b0a363648ea1156c1927390999ee1339edace
DNT-Khoa <khoa.coder.jedi@gmail.com>
2026-08-26 19:23:06 +0200

    chore: remove old Python solutions for two-sum and concatenation-of-array
    Superseded by the Java-based leetknight workflow; these will be
    re-added in Java as needed.

--- concatenation-of-array/solution.py
+++
@@ -1,9 +0,0 @@
-# Time:  O(n)
-# Space: O(n)
-
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        concat_lst = []
-        concat_lst.extend(nums)
-        concat_lst.extend(nums)
-        return concat_lst
